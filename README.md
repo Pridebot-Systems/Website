2f38a655fc4520766f353f535b42cbb29d18e3d9
Sdriver1 <sjdriver08@gmail.com>
2025-05-10 13:38:23 -0400

    Crazy amount of changes

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# portalnetwork
+# Pridebot Website
+
+**Sites**:
+- https://pridebot.xyz | Home
+- https://pridebot.xyz/
+- https://pfp.pridebot.xyz
+
+**Redirects**:
+
+
+**Legal**:
+- [Privacy Policy](https://pridebot.xyz/privacy)
+- [Terms of Service](https://pridebot.xyz/terms)
--- public/assets/js/server.mjs
+++ public/assets/js/server.mjs
@@ -7,39 +7,52 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const port = 8081;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/assets", express.static(join(__dirname, "../../../public/assets")));
 
 const serverStart = new Date();
-const serverStartformat = Math.floor(serverStart.getTime() / 1000); 
+const serverStartformat = Math.floor(serverStart.getTime() / 1000);
 
-const serveStatic = (route, directory) => {
-  app.use(route, express.static(join(__dirname, directory), { index: false }));
-};
-serveStatic("/assets/css", "../../assets/css");
-serveStatic("/assets/js", "../../assets/js");
-serveStatic("/assets/images", "../../assets/images");
-serveStatic("/assets/fonts", "../../assets/fonts");
-serveStatic("/assets/bootstrap", "../../assets/bootstrap");
+// Sites
+app.get("/", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/sites/index.html"))
+);
+app.get("/morbius", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/sites/morbius.html"))
+);
+app.get("/partners", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/sites/partners.html"))
+);
+app.get("/profiles/:userId", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/sites/profiles.html"))
+);
 
-const serveHTML = (route, file) => {
-  app.get(route, (req, res) => res.sendFile(join(__dirname, file)));
-};
+// Redirects
+app.get("/github", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/redirects/github.html"))
+);
+app.get("/github/organization", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/redirects/githuborg.html"))
+);
+app.get("/invite", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/redirects/invite.html"))
+);
+app.get("/premium", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/redirects/premium.html"))
+);
+app.get("/support", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/redirects/support.html"))
+);
 
-const htmlRoutes = [
-  { route: "/", file: "../../index.html" },
-  { route: "/github", file: "../../github.html" },
-  { route: "/github/organization", file: "../../githuborg.html" },
-  { route: "/tos", file: "../../tos.html" },
-  { route: "/privacy", file: "../../privacy.html" },
-  { route: "/morbius", file: "../../morbius.html" },
-  { route: "/invite", file: "../../invite.html" },
-  { route: "/premium", file: "../../premium.html" },
-  { route: "/support", file: "../../support.html" },
-  { route: "/partners", file: "../../partners.html" },
-  { route: "/profiles/:userId", file: "../../profiles.html" },
-  { route: "/survey", file: "../../review.html" },
-];
-
-htmlRoutes.forEach((route) => serveHTML(route.route, route.file));
+// Legal
+app.get("/tos", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/legal/tos.html"))
+);
+app.get("/privacy", (req, res) =>
+  res.sendFile(join(__dirname, "../../../public/legal/privacy.html"))
+);
 
 app.get("/:imageName", (req, res) => {
   const imageName = req.params.imageName;
